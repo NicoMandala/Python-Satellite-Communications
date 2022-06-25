4c9dc50ac91ceb9c990c1c287d982b1f9883fb5a
Chaitanya Mandala <35036701+NicoMandala@users.noreply.github.com>
2022-06-25 11:51:42 +0200

    Update groundstation.py

--- groundstation.py
+++ groundstation.py
@@ -1,21 +1,12 @@
-class Antenna:
-    """Instatiate a ground station with all its parameters
-    Gain,EIRP, Usable Frequencies, Efficiencies, Dish Diameter"""
-
-    def __init__(self,dia,EIRP,eta,freq):
-        self.dia = 70 # diameter
-        self.EIRP = 1000 # effective isotropic radiated power
-        self.freq = 27*10^9 # frequency used in the radar
-        self.eta = 0.55 # overall efficiency of the radar
-
-    def Gain(self):
-        return 10*log10(self.eta*(pi*self.dia*self.freq/c)^2)
 
 
 class Groundstation:
     # Ground stations are defined based on three parameters latitude, longitude and height
     # Possible ideas for next versions: choose the central body
-    def __init__(self,lat,long,height):
-        self.lat = 0
-        self.long = 0
-        self.height = 0
+    # The default values represent the Cebreros station 
+    
+    def __init__(self,lat,long,height,elev):
+        self.lat    = 40.4535 # Latitude in degrees
+        self.long   =-4.3683  # Longitude in degrees
+        self.height = 794     # height in metres    
+        self.elev   = 10      # elevation in degrees
