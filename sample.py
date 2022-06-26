24147996b911966df7b702f6b27a19c171555783
Chaitanya Mandala <35036701+NicoMandala@users.noreply.github.com>
2022-06-26 18:58:15 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,3 +2,11 @@
 
 Canberra = Groundstation(355.7485824,40.4299749,0.849874,15) # lat,long,height,elevation
 print(Canberra.lat,Canberra.long,Canberra.height,Canberra.elev)
+
+
+# Using text2signal
+# Run the program, it prompts for a input and generates a binary plot
+
+# Using Antenna class
+ant =  Antenna(70,1000,27*10^9,0.55) # diameter, EIRP, frequency, total antenna efficiency
+print(ant.dia, ant.EIRP, ant.freq, ant.eta)
