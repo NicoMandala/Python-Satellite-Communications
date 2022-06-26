b2a772fcf0afb6b8f03efe68ddf4df1d64b7b99a
Chaitanya Mandala <35036701+NicoMandala@users.noreply.github.com>
2022-06-26 18:50:22 +0200

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,4 @@
+# Using ground station class
+
+Canberra = Groundstation(355.7485824,40.4299749,0.849874,15) # lat,long,height,elevation
+print(Canberra.lat,Canberra.long,Canberra.height,Canberra.elev)
