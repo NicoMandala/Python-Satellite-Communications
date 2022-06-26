77b2b3e498416d7b87d2eb4a6126ea249cc09ca7
Chaitanya Mandala <35036701+NicoMandala@users.noreply.github.com>
2022-06-26 18:58:34 +0200

    Update antenna.py

--- antenna.py
+++ antenna.py
@@ -5,10 +5,10 @@ class Antenna:
     Gain,EIRP, Usable Frequencies, Efficiencies, Dish Diameter"""
 
     def __init__(self,dia,EIRP,eta,freq):
-        self.dia = 70 # diameter
-        self.EIRP = 1000 # effective isotropic radiated power
-        self.freq = 27*10^9 # frequency used in the radar
-        self.eta = 0.55 # overall efficiency of the radar
+        self.dia  = dia # diameter
+        self.EIRP = EIRP # effective isotropic radiated power
+        self.freq = freq # frequency used in the radar
+        self.eta  = eta  # overall efficiency of the radar
 
     def Gain(self):
         return 10*log10(self.eta*(pi*self.dia*self.freq/c)^2)
